b58b60866fbefd59cb47067cab169da7a6160dba
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-08 05:05:11 -0700

    created '5-printf.c' C Program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -12,7 +12,7 @@ int main(void)
 	 * use printf to output a
 	 * string to the stdout
 	 */
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
