055c44b1347922b6c5b6f508b6e228e50f93cf79
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-08 05:48:36 -0700

    created '101-quote.c' C Program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@ int main(void)
 	 * print a string without using
 	 * printf or puts
 	 */
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
 	return (1);
