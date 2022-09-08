950d62571f37b9f054e1c9d32fb1d17cf2f27ae8
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-08 04:43:07 -0700

    created '4-puts.c' C Program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+/*
+ * The 'main' function is the entry 
+ * point with a return value of 0
+ * to indicate success.
+ */
 
 int main(void){
-    /*
-     * print a sentence using the
-     * 'puts' function
-     */
+	/*
+	 * print a sentence using the
+	 * 'puts' function
+	 */
 
-    puts("Programming is like building\
-          a multilingual puzzle\n");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle\"\n");
+	
+	return (0);
 }
