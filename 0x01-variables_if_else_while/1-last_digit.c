d0a9aef53ab04364634a5f5de96012045f9a9ed3
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-09 03:47:10 -0700

    created '1-last_digit.c' C Program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,8 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	int last = n % 10;
+	int last;
+	last = n % 10;
 
 	printf("Last digit of %d is %d and is ", n, last);
 	if (last > 5)
