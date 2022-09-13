43530a7f47a531c7a4e4daddd20f9e0ca09c94cb
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-13 13:40:26 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,42 +11,39 @@ void print_times_table(int n)
 {
 	int tot, incr, num = 0;
 
-	while (num++ < n && n < 15 && n > -1)
+	if (n < 15 && n > -1)
 	{
-		incr = -1;
-		while (incr++ < n)
+		while (num++ < n)
 		{
-			tot = num * incr;
-			if (tot > 99)
+			incr = -1;
+			while (incr++ < n)
 			{
-				_putchar((tot / 100) + '0');
-				_putchar((tot / 10) % 10 + '0');
-				_putchar((tot % 10) + '0');
-			} else if (tot > 9)
-			{
-				_putchar((tot / 10) + '0');
-				_putchar((tot % 10) + '0');
-			} else if (tot < 10)
-			{
-				_putchar((tot % 10) + '0');
+				tot = num * incr;
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (tot < 10 && num != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((tot % 10) + '0');
+				} else if (tot >= 10 && tot < 100)
+				{
+					_putchar(' ');
+					_putchar((tot / 10) + '0');
+					_putchar((tot % 10) + '0');
+				} else if (tot >= 100 && incr != 0)
+				{
+					_putchar((tot / 100) + '0');
+					_putchar((tot / 10) % 10 + '0');
+					_putchar((tot % 10) + '0');
+				} else
+					_putchar((tot % 10) + '0');
 			}
-			if ((tot + num) < 10 && incr < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-			} else if ((tot + num) < 100 && incr < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			} else if (incr < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(10);
 		}
-		_putchar(10);
 	}
 }
