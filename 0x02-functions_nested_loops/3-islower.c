502b005c5b97c59cfb08b4a351665d3a1486a74e
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-13 03:39:19 -0700

    new fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,29 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int _islower(int c);
-
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-
-
-/**
  * _islower - determines lowercase
  * alphabet
  *
