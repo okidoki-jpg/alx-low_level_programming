0cf4d3bc7c924c15314e850ccff44968b4ac8e60
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-13 10:42:10 -0700

    created '11-print_to_98.c' C Program

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,14 @@
 
 void print_to_98(int n)
 {
-	while (!(n == 99))
+	while (!(n == 98))
 	{
 		if (n < 99)
 			printf("%d", n++);
 		else if (n > 98)
 			printf("%d", n--);
-		if (!(n == 98))
-			printf(", ");
+		printf(", ");
 	}
+	puts(98);
 	putchar(10);
 }
