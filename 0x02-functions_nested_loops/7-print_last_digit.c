fb96a5c7ab7bb5962f061d09f164c2f7c714c2f0
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-13 07:23:20 -0700

    created '7-print_last_digit.c' C Program

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,11 @@
 
 int print_last_digit(int n)
 {
-	if (!(n > 0))
+	n = (n % 10)
+	if (n < 0)
 	{
 		n *= -1;
 	}
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	_putchar(n + '0');
+	return (n);
 }
