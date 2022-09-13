6683869e999b328001f7456a31fd2d4896303361
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-13 03:27:25 -0700

    new fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-
-/**
  * print_alphabet - prints the alphabet
  *
  * Return: void
