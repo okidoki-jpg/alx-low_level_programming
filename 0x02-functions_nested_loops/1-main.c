a9b7c3184dcc32eca90f37ce3a199b2588e9367f
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-13 03:11:46 -0700

    main.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -13,21 +13,3 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-
-
-/**
- * print_alphabet - prints the alphabet
- *
- * Return: void
- */
-
-void print_alphabet(void)
-{
-	int idx = 97;
-
-	while (idx < 123)
-	{
-		_putchar(idx);
-	}
-	_putchar(10);
-}
