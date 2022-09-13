159b593e5ce20a91ea987c7c50d3ae13dd195272
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-13 08:06:27 -0700

    created '8-24_hours.c' C Program

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,20 +9,20 @@
 
 void jack_bauer(void)
 {
-	int hr_t = 0;
+	int hr_t = -1;
 	int hr_u;
 	int min_t;
 	int min_u;
 
 	while (hr_t++ < 2)
 	{
-		hr_u = 0;
+		hr_u = -1;
 		while (hr_u++ < 9)
 		{
-			min_t = 0;
+			min_t = -1;
 			while (min_t++ < 5)
 			{
-				min_u = 0;
+				min_u = -1;
 				while (min_u++ < 9)
 				{
 					_putchar(hr_t + '0');
