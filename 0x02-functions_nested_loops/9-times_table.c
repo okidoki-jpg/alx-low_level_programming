39be1d491bb6643bc4af77e0ef975136c46a0523
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-13 10:06:51 -0700

    created '9-times_table.c' C Program

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 			{
 				_putchar((tot % 10) + '0');
 			}
-			if ((tot + (incr -1)) < 10 && incr < 9)
+			if ((tot + num) < 10 && incr < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
