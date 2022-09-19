f9bd34e7c7953d42c83f43cd8de0a3781d33bf33
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-19 03:08:32 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - update int pointer value to 98
+ * @*n: integer pointer to be updated
+ * 
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0-reset_to_98.c:
+    update an integer pointer value to 98
+
