0e077c9dd911d99ec3d2609bc1f3bdbabb67500d
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-21 00:03:17 -0700

    test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,11 +9,11 @@
 
 void print_times_table(int n)
 {
-	int i, j, res;
+	int j, res;
 
 	if (n < 0 || n > 15)
 		return;
-	for (i = 0; i <= n; i++)
+	for (int i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
