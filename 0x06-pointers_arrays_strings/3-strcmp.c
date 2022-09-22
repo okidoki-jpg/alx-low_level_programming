d8cc6fab192c7bf9e9f38014ed9efe2b89db70ca
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-22 01:21:00 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,10 +10,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int res;
 
-	while (*s1 != NULL && *s1 == *s2)
+	while (*s1 && *s1 == *s2)
 	{
-		s1++;
-		s2++;
+		++s1;
+		++s2;
 	}
 	res = (int) *s1 - (int) *s2;
 	return (res);
