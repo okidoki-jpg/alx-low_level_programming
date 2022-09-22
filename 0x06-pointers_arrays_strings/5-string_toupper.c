ff1196f374e76ad2aed0d3866e58afce7da9b5e5
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-22 02:00:59 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,13 @@ char *string_toupper(char *src)
 {
 	int i = 0;
 
-	while (src[i])
+	while (src[i] != '\0')
 	{
 		if (src[i] > 96 && src[i] < 123)
 		{
 			src[i] -= 32;
-			i++;
 		}
+		i++;
 	}
 	return (src);
 }
