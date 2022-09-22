88131d11d0e39b668c0345dd0377db7dde7a21fe
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-22 01:34:24 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * reverse_array - reverse a string
  * @a: array to reverse
@@ -11,7 +9,7 @@
 
 void reverse_array(int *a, int n)
 {
-	int end = n, front, tmp;
+	int end = n - 1, front, tmp;
 
 	/**
 	 * move front index foward and swap values
