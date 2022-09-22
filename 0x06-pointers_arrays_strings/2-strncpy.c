d08404fe22b67d4dcdfa5105721f1808c35b8177
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-22 00:33:17 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,10 +9,10 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (src[i] != '\0' && i < n)
-		dest[i] = src[i++];
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
 	for (; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
