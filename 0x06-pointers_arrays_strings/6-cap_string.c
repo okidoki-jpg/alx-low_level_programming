3b38e714eda44a9c6ded54bc49526c97140f54cb
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-22 03:21:22 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,18 +7,18 @@
 
 char *cap_string(char *src)
 {
-	int i = 0;
+	int i = 0, j;
+	char sp[] = " \t\n,;.?!\"(){}";
 
 	while (src[i] != '\0')
 	{
-		if ((src[i - 1] == 32 || src[i - 1] == '\t' ||
-			src[i - 1] == '\n' || src[i - 1] == ',' ||
-			src[i - 1] == ';' || src[i - 1] == '.' ||
-			src[i - 1] == '?' || src[i - 1] == '!' ||
-			src[i - 1] == '\"') &&
-			(src[i] >= 'a' && src[i] <= 'z'))
+		for (j = 0; sp[j] != '\0'; j++)
 		{
-			src[i] -= 32;
+			if (src[i - 1] == sp[j] &&
+				(src[i] >= 'a' && src[i] <= 'z'))
+			{
+				src[i] -= 32;
+			}
 		}
 		i++;
 	}
