1f71bd60bb8279d09191b4bddcad3be3cfd96727
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-26 06:25:20 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ * _memset - assign a constant value to a specified
+ * amount of locations in memory
+ * @s: area pointed to in memory
+ * @b: constant byte
+ * @n: number of bytes
+ *
+ * Return: pointer to changed array 's'
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = -1;
+
+	while (i++ < n)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0-memset.c:
+    impliment memset function
+
