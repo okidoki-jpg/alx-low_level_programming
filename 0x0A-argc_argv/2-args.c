7da33e7c9a6f0578170049bf3807252e3e19356c
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-30 11:43:22 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print program arguements
+ * @argc: number of program arguements
+ * @argv: argument vector (all program arguements)
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		while(argc--)
+		{
+			printf("%s\n", *argv++);
+		}
+	}
+	(void) argv;
+	return (0);
+}
