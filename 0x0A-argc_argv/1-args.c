dfdf49f1149c57a294fc1221332fd05b7d48cac6
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-09-30 11:31:42 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print number of program arguements
+ * @argc: number of program arguements
+ * @argv: argument vector (all program arguements)
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	(void) argv;
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,6 @@
 0-whatsmyname.c:
     print program name
 
+1-args.c:
+    print number of program arguments
+
