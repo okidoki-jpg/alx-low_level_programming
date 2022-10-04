cdc336a3e1fe62c5e239ca0a8155dcb27c851ad3
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-10-04 00:40:23 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,13 +8,16 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *lis;
+	char *lis =  '\0';
 	int i;
 
 	if (size < 1)
 		return ('\0');
 
 	lis = malloc(sizeof(char) * size);
+	if (lis == '\0')
+		return ('\0');
+
 	for (i = 0; i < size; i++)
 	{
 		lis[i] = c;
