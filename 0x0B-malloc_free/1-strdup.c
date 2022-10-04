1b88c49e2b0fb08b85bb765fc6e8fb1773684312
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-10-04 01:26:54 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - copy string to newly allocated memory
+ * @str: string to copy
+ *
+ * Return: char pointer if size > 0, else NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *copy =  NULL;
+	unsigned int idx, size;
+
+	for (size = 0; str[size], size++)
+		continue;
+
+	copy = malloc(sizeof(char) * size);
+	if (copy == NULL)
+		return (NULL);
+
+	for (idx = 0; idx < size; idx++)
+	{
+		copy[idx] = str[idx];
+	}
+	return (copy);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,6 @@
 0-create_array.c:
     create & initialize dynamic array
 
+1-strdup.c:
+    copy string to newly allocated memory
+
