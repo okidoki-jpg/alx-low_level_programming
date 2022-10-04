8d4b95f389f67511f246f4e01765d1e2099f2896
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-10-04 02:24:48 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,13 +11,31 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *cat =  NULL;
-	unsigned int len1, len2, idx;
+	unsigned int len1 = NULL, len2 = NULL, idx;
 
-	for (idx = 0; s1[idx]; len1 = idx++)
-		continue;
+	if (s1 != NULL)
+	{
+		for (idx = 0; s1[idx]; len1 = idx++)
+			continue;
+	}
+	else
+	{
+		len1 = 0;
+	}
 
-	for (idx = 0; s2[idx]; len2 = idx++)
-		continue;
+	if (s2 != NULL)
+	{
+		for (idx = 0; s2[idx]; len2 = idx++)
+			continue;
+	}
+	else
+	{
+		s2 = '\0';
+		len2 = 0;
+	}
+
+	if (len1 == len2 == 0)
+		return (NULL);
 
 	cat = malloc(sizeof(char) * (len1 + len2));
 	if (cat == NULL)
@@ -33,7 +51,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (idx = 0; idx <= len2; idx++)
 	{
-		cat[idx] = s2[idx];
+		cat[len1 + idx] = s2[idx];
 	}
 	return (cat);
 }
