f61c9edba47ba54aeb81a7720b360a5c84710993
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2022-10-04 10:24:34 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -39,7 +39,7 @@ char *str_concat(char *s1, char *s2)
 			cat[idx] = s1[idx];
 		}
 		if (!(s2))
-			cat[idx++] = '\0';
+			cat[idx] = '\0';
 	}
 
 	if (s2)
@@ -48,6 +48,7 @@ char *str_concat(char *s1, char *s2)
 		{
 			cat[len1 + idx] = s2[idx];
 		}
+		cat[idx] = '\0';
 	}
 	return (cat);
 }
