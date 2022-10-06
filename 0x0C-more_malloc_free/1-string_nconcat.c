bd8c869cfa9efdfc8fb0586e105655ac3d75daa9
sonofpeter-exe <sonofpeter.exe@gmail.com>
2022-10-06 15:53:30 +0200

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (!(s1))
 	{
-		s1 = "";
+		s1 = " ";
 	}
 	else
 	{
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (!(s2))
 	{
-		s2 = "";
+		s2 = " ";
 	}
 	else
 	{
