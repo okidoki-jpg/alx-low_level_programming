146e3ad8cea024140470c7d63fb988e67eabfcbe
sonofpeter-exe <sonofpeter.exe@gmail.com>
2022-10-07 07:42:42 +0200

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	range = max - min;
+	range = max - (min);
 	arr = malloc(sizeof(int) * range);
 	if (!arr)
 		return (NULL);
