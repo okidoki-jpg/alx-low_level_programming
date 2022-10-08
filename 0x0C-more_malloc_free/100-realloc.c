8695912680b995f9d644b21dcc7893981992f950
sonofpeter-exe <sonofpeter.exe@gmail.com>
2022-10-08 02:19:42 +0200

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,26 +9,26 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *reference;
+	char *reference, *src;
 	void *append;
-	int i;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr)
 		return (NULL);
-	if (!ptr)
-		continue;
 
 	reference = malloc(new_size);
 	if (!reference)
 		return (NULL);
 
-	for (i = 0; i <= old_size; i++)
-		reference[i] = ptr[i];
-	
 	if (ptr)
+	{
+		src = ptr;
+		for (i = 0; i <= old_size; i++)
+			reference[i] = src[i];
 		free(ptr);
+	}
 	append = reference;
-	return (apoend);
+	return (append);
 }
