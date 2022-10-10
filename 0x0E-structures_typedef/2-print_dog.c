755e2eef3ffb5848e86f31115b9baf5d48c78778
sonofpeter-exe <sonofpeter.exe@gmail.com>
2022-10-10 14:33:15 +0200

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,16 +15,16 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)\n");
+			printf("(nil)\n");
 
 		if (d->age)
 			printf("Age: %f\n", d->age);
 		else
-			printf("Age: (nil)\n");
+			printf("(nil)\n");
 
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)\n");
+			printf("(nil)\n");
 	}
 }
