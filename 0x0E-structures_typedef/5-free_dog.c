076f78b5f5f22e5c202148c66a0969cdc9f4cb63
sonofpeter-exe <sonofpeter.exe@gmail.com>
2022-10-10 17:29:51 +0200

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+/**
+ * free_dog - free dog attributes
+ * @d: dog object
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d->d);
+	}
+}
