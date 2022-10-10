dacf1a8c568a282c7a0c2d4b098b931399d886e2
sonofpeter-exe <sonofpeter.exe@gmail.com>
2022-10-10 16:50:53 +0200

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include "dog.h"
 
+int len(char *str);
+char *cpy(char *dest, char *str);
+
 /**
  * new_dog - create new dog object
  * @name: new dog name
@@ -14,14 +17,56 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
+	int name_len, own_len;
 
 	doggo = malloc(sizeof(dog_t));
 	if (!doggo)
 		return (NULL);
 
-	doggo->name = name;
+	name_len = len(name);
+	own_len = len(owner);
+
+	doggo->name = malloc(sizeof(char) * name_len + 1);
+	if (doggo->name == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo->owner = malloc(sizeof(char) * own_len + 1);
+	if (doggo->owner == NULL)
+	{
+		free(doggo->name);
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo->name = cpy(doggo->name, name);
 	doggo->age = age;
-	doggo->owner = owner;
+	doggo->owner = cpy(doggo->owner, owner);
 
 	return (doggo);
+}
+
+int len(char *str)
+{
+	int c = 0;
+
+	while (*str)
+	{
+		c++;
+		str++;
+	}
+	return (c);
+}
+
+char *cpy(char *dest, char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		dest[i] = str[i];
+	dest[i] = '\0';
+
+	return (dest);
 }
