4572aa87b34b534996cc81bfcabb90b6e42663d9
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-12 12:01:50 +0200

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_name - pointer function that prints name
+ * @name: name to print
+ * @f: function parameter
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+0-print_name.c:
+    print name from pointer to function
+
