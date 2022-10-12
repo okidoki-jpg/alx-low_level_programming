0a2de14e8b0bfe045ceb2296480be6e72d0ba560
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-12 13:05:13 +0200

    1-array_iterator.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef __FUNCHEADS__
 #define __FUNCHEADS__
 
-typedef size_t unsigned int
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
