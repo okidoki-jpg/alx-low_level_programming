a114380b76e6330a28fe6ae4c6e08860425c9e33
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-12 19:18:00 +0200

    calc

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * op_add - addition function
@@ -53,7 +54,7 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	if (!b)
+	if (!b || !a)
 	{
 		printf("Error\n");
 		exit(100);
@@ -72,7 +73,7 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	if (!b)
+	if (!b || !a)
 	{
 		printf("Error\n");
 		exit(100);
