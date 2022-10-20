a52485e8f62e883e6fac9f8b6eb3bc5c6550a82c
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-21 00:35:10 +0200

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * pre_exec - execute before main()
+ *
+ * Return: void
+ */
+
+void pre_exec(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
