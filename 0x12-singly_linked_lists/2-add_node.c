3cd0f3b49b27f77c35acb67b35c0d5e50e645cf5
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-20 20:40:58 +0200

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_node - add node to linked list
+ * @head: next node
+ * @str: string to append
+ *
+ * Return: address to new element, null on fail
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t len = _strlen(str);
+	list_t *node = malloc(sizeof (list_t));
+
+	if (!node)
+		return (NULL);
+
+	node->str = strdup(str);
+	node->len = len;
+	node->next = *head;
+	*head = node;
+
+	return (node);
+}
+
+/**
+ * _strlen - determine string length
+ * @str: string to evaluate
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*str)
+		i++;
+	str++;
+
+	return (i);
+}
