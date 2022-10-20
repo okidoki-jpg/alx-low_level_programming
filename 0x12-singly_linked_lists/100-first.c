278231f1bb7e7fa68ff05baaa3c40874079231f1
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-21 00:36:59 +0200

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,6 @@
 
 void pre_exec(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
