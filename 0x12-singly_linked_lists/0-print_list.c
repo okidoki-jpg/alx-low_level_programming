28954ce523349d572e6bc9d71472cfec127b8e8c
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-20 19:10:23 +0200

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,21 +9,21 @@
 
 size_t print_list(const list_t *h)
 {
+	const list_t *idx;
 	int count = 0;
 
 	if (!h)
-	{
-		printf("[0] (nil)\n");
 		return (0);
-	}
-	while (h)
+	
+	idx = h;
+	while (idx)
 	{
 		count++;
-		if (!h->str)
+		if (!idx->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+			printf("[%d] %s\n", idx->len, idx->str);
+		idx = idx->next;
 	}
 	return (count);
 }
