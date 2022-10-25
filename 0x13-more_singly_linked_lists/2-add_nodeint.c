8468db1338ba8ec65e686f7525fd1a42f7e18cd3
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-25 13:30:22 +0200

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,8 +11,7 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *node;
-	node = malloc(sizeof(listint_t));
+	listint_t *node = malloc(sizeof(listint_t));
 
 	if (!node)
 		return (NULL);
