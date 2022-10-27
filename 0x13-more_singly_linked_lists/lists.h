410d360f8a6a442e0fa8060b7c364b7d78f6b103
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-27 11:29:57 +0200

    testing 0x13-more_singly_linked_lists/0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,5 @@
-#ifndef _MAIN_H /* You may change this (e.g. main.h becomes MAIN_H) */
+#ifndef _MAIN_H 
+/* You may change this (e.g. main.h becomes MAIN_H) */
 #define _MAIN_H /* You may change this (e.g. main.h becomes MAIN_H) */
 
 #include <stdio.h>
