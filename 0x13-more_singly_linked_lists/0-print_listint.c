bd4248775e297f0b432fc589381ea886fb661c51
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-27 10:17:53 +0200

    test 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,10 +8,12 @@
  * Return: the number of nodes.
  */
 
-size_t print_listint(listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t size = 1;
 
+	if (h == NULL)
+		return (0);
 
 	while (h->next)
 	{
