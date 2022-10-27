c6e8629b5c6a8664156dc38eb67f0a3142447630
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-27 10:53:06 +0200

    testing 0x13-more_singly_linked_lists/0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,6 +10,7 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct listint_s
 {
 	int n;
@@ -17,7 +18,7 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
-size_t print_listint(listint_t *h);
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
