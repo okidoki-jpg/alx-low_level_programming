65ad47e6487ddceb4a24157e17ba44f91eb5435c
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-27 11:41:32 +0200

    testing 0x13-more_singly_linked_lists/5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - clears a singly liked list.
+ * @head: Singly linked list head pointer
+ *
+ * Description: deletes all the nodes of the singly linked list
+ * Return: void (Nothing)
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while ((*head) != NULL)
+	{
+		temp = (*head);
+		(*head) = (*head)->next;
+		free(temp);
+	}
+
+	temp = NULL;
+}
