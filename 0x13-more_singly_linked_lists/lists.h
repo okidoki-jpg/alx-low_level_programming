071d6c214f59e5ab1f13dc88ff331d6cbf8b6e65
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-27 09:49:57 +0200

    test 0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
 #define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
 
+
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
