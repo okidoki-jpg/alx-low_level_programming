735a7774a1ec267170e72419e08993ed6171d881
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-27 11:35:26 +0200

    testing 0x13-more_singly_linked_lists/0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,7 +15,7 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
-size_t print_listint(listint_t *h);
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
