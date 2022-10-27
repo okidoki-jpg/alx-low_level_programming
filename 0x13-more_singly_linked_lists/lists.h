929aae7594ba7bbb6f05cdd7b8367a36bbf5f237
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-27 11:33:36 +0200

    testing 0x13-more_singly_linked_lists/0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H) */
-
-#include <stdio.h>
-#include <stdlib.h>
+#define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
 
 /**
  * struct listint_s - singly linked list
@@ -11,14 +8,14 @@
  *
  * Description: singly linked list node structure
  */
-
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
+int _putchar(char c);
+size_t print_listint(listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
