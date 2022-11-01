faa6197e5e2f01880c68799adce6e1bef8c5f789
okidoki-jpg <okuhlen.20@gmail.com>
2022-11-01 10:59:28 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * cteate_file - create a file
- * @filename: name of file to creatw
+ * create_file - create a file
+ * @filename: name of file to create
  * @text_content: content to populate file
  *
  * Return: 1 on success, -1 on failure
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -29,6 +29,4 @@ int create_file(const char *filename, char *text_content)
 	}
 	close(fd);
 	return (1);
-
-
 }
