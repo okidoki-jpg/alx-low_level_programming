8e6bd0adc5c87687cc4c7c21f1e2d02778ef0960
okidoki-jpg <okuhlen.20@gmail.com>
2022-11-02 01:57:03 +0200

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,13 +19,13 @@ int main(int ac, char **av)
 	if (ac != 3)
 		errorMsg("Usage: %s file_from file_to\n", av[0], 97);
 
-	fd_src = open(av[1], O_RDONLY);
-	if (fd_src == -1)
-		errorMsg("Error: Can't read from file %s\n", av[1], 98);
-
 	fd_dest = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_dest == -1)
 		errorMsg("Error: Can't write to %s\n", av[2], 99);
+
+	fd_src = open(av[1], O_RDONLY);
+	if (fd_src == -1)
+		errorMsg("Error: Can't read from file %s\n", av[1], 98);
 
 	len = read(fd_src, buff, sizeof(buff));
 	while (len > 0)
