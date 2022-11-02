4d83d0d86438aeb3e29fb8238e13194f7647b5a2
okidoki-jpg <okuhlen.20@gmail.com>
2022-11-02 02:22:58 +0200

    100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *	main - program entry point
+ *	@ac: argument count
+ *	@av: argument vector
+ *	
+ *	Return: 0 on success
+ */
+
+int main(int ac, char **av)
+{
+	(void) ac;
+	(void) av;
+	return (0);
+}
