c4e7593ebd6c0e26c51e5b405adbceee7bd37496
Okuhle <okuhlen.20@gmail.com>
2023-01-12 16:47:46 +0000

    C 0x1A: added 3 & 4

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,57 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - set hash table value
+ * @ht: hash table
+ * @key: index key
+ * @value: key's value
+ *
+ * Return: 1 on success, else 0
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+	unsigned long int idx = key_index(key, ht->size);
+
+	if (!node)
+		return (0);
+
+	node->key = (char *) key;
+	node->value = (char *) value;
+
+	if (!ht->array[idx])
+		ht->array[idx] = node;
+	else
+		ht = collision(ht, key, value);
+
+	return (1);
+}
+
+/**
+ * collision - handle collision
+ * @ht: hash table
+ * @key: index key
+ * @value: key's value
+ *
+ * Return: modified hash table
+ */
+
+hash_table_t *collision(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx = key_index(key, ht->size);
+
+	hash_node_t *tail = ht->array[idx];
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (!node)
+		return (NULL);
+
+	node->key = (char *) key;
+	node->value = (char *) value;
+	node->next = tail;
+
+	ht->array[idx] = node;
+
+	return (ht);
+}
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve value from hash table
+ * @ht: hash table
+ * @key: index key for table
+ *
+ * Return: value, else NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node = ht->array[key_index(key, ht->size)];
+
+	while (node)
+	{
+		if (!strcmp(node->key, key))
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
