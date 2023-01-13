0e24a1467e7268dec7e9188a71448f5d00a833ae
Okuhle <okuhlen.20@gmail.com>
2023-01-13 16:28:41 +0000

    fixed 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		node->next = ht->array[idx];
-		ht-array[idx] = node;
+		ht->array[idx] = node;
 	}
 	return (1);
 }
