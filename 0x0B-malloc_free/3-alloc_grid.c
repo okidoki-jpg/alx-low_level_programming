91cdab0bbfe44f0bb8667289a02ee196f20d73dc
Okuhle Nsibande <sonofpeter.exe@gmail.com>
2023-04-09 14:58:48 +0000

    patched 3 & 4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,30 +11,29 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **arr;
+	int **grid;
 	int i, j;
-	if (width < 1 || height < 1)
+
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = malloc(height * sizeof(int));
-	if (arr == NULL)
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(width * sizeof(int));
-		if (arr[i] == NULL)
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
-				free(arr[j]);
-			free(arr);
+				free(grid[j]);
+			free(grid);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
-		{
-			arr[i][j] = 0;
-		}
+			grid[i][j] = 0;
 	}
 
-	return (arr);
+	return (grid);
 }
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,6 +1,16 @@
+0-create_array.c:
+    create an array of chars, and initialize it with a specific char
+
+1-strdup.c:
+    Return a pointer to a newly allocated space in memory, which
+    contains a copy of the string given as a parameter.
+
 2-str_concat.c:
     concatenate two strings to new memory
 
 3-alloc_grid.c:
     create, and allocate 0 to, a 2D array
 
+4-free_grid.c:
+    Free a 2 dimensional grid previously created by the alloc_grid
+    function.
